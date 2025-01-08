5d1898d74a61385a82644335d24e4068d321c3f2
Sina Arasteh <sinna.arasteh@gmail.com>
2025-01-08 13:47:45 +0330

    Create the 'MailingAddressForm'

--- entry_form/forms.py
+++ entry_form/forms.py
@@ -1,7 +1,15 @@
 from django import forms
 from . import models
 
+
+class MailingAddressForm(forms.ModelForm):
+    class Meta:
+        model = models.MailingAddress
+        fields = '__all__'
+
+
 class EntrantsForm(forms.ModelForm):
     class Meta:
         model = models.Entrants
         fields = '__all__'
+
