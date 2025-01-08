1bd49ff787a4cf35626b4bc11461603f158f616b
Sina Arasteh <sinna.arasteh@gmail.com>
2025-01-08 23:49:59 +0330

    Modify the 'forms' module

--- entry_form/forms.py
+++ entry_form/forms.py
@@ -2,14 +2,38 @@ from django import forms
 from . import models
 
 
-class MailingAddressForm(forms.ModelForm):
-    class Meta:
-        model = models.MailingAddress
-        fields = '__all__'
+# class MailingAddressForm(forms.ModelForm):
+#     class Meta:
+#         model = models.MailingAddress
+#         fields = '__all__'
 
 
-class EntrantsForm(forms.ModelForm):
-    class Meta:
-        model = models.Entrants
-        fields = '__all__'
+# class EntrantsForm(forms.ModelForm):
+#     class Meta:
+#         model = models.Entrants
+#         fields = '__all__'
+
+class EntryForm(forms.Form):
+    last_name = forms.CharField(max_length=33)
+    first_name = forms.CharField(max_length=33, required=False)
+    middle_name = forms.CharField(max_length=33, required=False)
+    gender = forms.CharField(max_length=6)
+    birth_date = forms.DateField()
+    birth_city = forms.CharField(max_length=33, required=False)
+    birth_country = forms.CharField(max_length=40)
+    eligibility_country = forms.CharField(max_length=40, required=False)
+    entrant_photograph = forms.ImageField()
+    in_care_of = forms.CharField(max_length=33, required=False)
+    address_line_one = forms.CharField(max_length=33)
+    address_line_two = forms.CharField(max_length=33, required=False)
+    city = forms.CharField(max_length=33)
+    province = forms.CharField(max_length=33)
+    zip_code = forms.CharField(max_length=33, required=False)
+    country = forms.CharField(max_length=40)
+    residence_country = forms.CharField(max_length=33)
+    phone_number = forms.CharField(max_length=33, required=False)
+    email_address = forms.EmailField()
+    education_level = forms.CharField(max_length=33)
+    marital_status = forms.CharField(max_length=85)
+    children_number = forms.IntegerField(min_value=0, max_value=20)
 
--- entry_form/views.py
+++ entry_form/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from .forms import EntrantsForm
+from .models import MailingAddress, Entrants
+from .forms import EntryForm
 import datetime
 
 
@@ -8,7 +9,23 @@ def begin_entry_view(request):
 
 def entry_form_view(request):
     if request.method == 'POST':
-        birth_date = datetime.date(request.POST['birth-year'], request.POST['birth-month'], request.POST['birth-day'])
-        pass
+        birth_date = datetime.date(int(request.POST['birth-year']), int(request.POST['birth-month']), int(request.POST['birth-day']))
+        updated_request = request.POST.copy()
+        updated_request.update({'birth_date': birth_date.isoformat()})
+        mailing_address_form = MailingAddressForm(request.POST)
+        mailing_address_form_unbound = MailingAddressForm()
+        entrant_form = EntrantsForm(updated_request)
+        entrant_form_unbound = EntrantsForm()
+        return render(request, "entry_form/testttt.html", {
+            'mailing_address_form': mailing_address_form,
+            'mailing_address_form_unbound': mailing_address_form_unbound,
+            'entrant_form': entrant_form,
+            'entrant_form_unbound': entrant_form_unbound,
+            'birth_date': birth_date,
+        })
+        # if mailing_address_form.is_valid():
+        #     print(mailing_address_form.cleaned_data)
+        #     print(entrant_form.cleaned_data)
+
 
     return render(request, 'entry_form/entry_form.html')
